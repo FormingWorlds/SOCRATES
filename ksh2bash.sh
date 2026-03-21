6879e2afbb3f366cbabfecfc87d3c07e3a04858b
Harrison Nicholls <harrison.nicholls@ast.cam.ac.uk>
2026-03-21 21:09:36 +0000

    Gitignore latex temp files. Remove ksh2bash script

--- ksh2bash.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Converting script shebang entries from ksh to bash"
-type ksh >/dev/null 2>&1 ||  sed -i 's/ksh/bash/g' set_rad_env
-type ksh >/dev/null 2>&1 ||  sed -i 's/ksh/bash/g' sbin/*
-
